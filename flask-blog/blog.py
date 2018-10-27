d9ff26a9e686988873d909e30a4b241127de2453
diango <maraberouz@hotmail.fr>
2018-10-27 18:19:04 +0000

    moche user login

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -5,6 +5,9 @@ import sqlite3
 
 # Configuration
 DATABASE = 'blog.py'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'rM\xb1\xdc\x12o\xd6i\xff+9$T\x8e\xec\x00\x13\x82.*\x16TG\xbd'
 
 app = Flask(__name__)
 
@@ -15,13 +18,30 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    error = None
+    status_code = 200
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+        request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid credentials. Please try again!!'
+            status_code = 401
+        else:
+            session['loged_in'] = True
+            return redirect(url_for('main'))
+    
+    return render_template('login.html', error = error), status_code
 
 @app.route("/main")
 def main():
-    return render_template("main.html")
+    return render_template('main.html')
+
+@app.route("/logout")
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out!')
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)
