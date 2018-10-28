fe5add71269cfee74c2f1d4bbbdac485dbcb7825
diango <maraberouz@hotmail.fr>
2018-10-28 13:10:17 +0000

    last update

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, \
 flash, redirect, url_for, g
+from functools import wraps
 
 import sqlite3
 
@@ -18,6 +19,16 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to log in first')
+            return redirect(url_for('login'))
+    return wrap
+
 @app.route("/", methods = ['GET', 'POST'])
 def login():
     error = None
@@ -35,7 +46,28 @@ def login():
 
 @app.route("/main")
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    cur = g.db.execute("SELECT * FROM posts")
+    posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts = posts)
+
+@app.route("/add", methods=['POST'])
+@login_required
+def add():
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash("All fields are required.... Please try again.")
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute('insert into posts(title, post) values(?, ?)',
+        [request.form['title'], request.form['post']])
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully posted!')
+        return redirect(url_for('main'))
 
 @app.route("/logout")
 def logout():
